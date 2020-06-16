2fc439d8cd7c7049b1b72e4c20b78dcd7616b7ea
Marlon-Urena <45586481+Marlon-Urena@users.noreply.github.com>
2020-06-16 19:48:29 +0100

    Created user model and user seed data.

--- database/models/user.js
+++ database/models/user.js
@@ -2,9 +2,12 @@ const Sequelize = require("sequelize");
 const db = require("../db");
 
 const User = db.define("user", {
-    userId: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
     firstName: { type: Sequelize.STRING, allowNull: false},
     lastName: { type: Sequelize.STRING, allowNull:false},
-    phoneNumber: {type: Sequelize.STRING(15), allowNull: true}
+    phoneNumber: {type: Sequelize.STRING(15), allowNull: true},
+    email: {type: Sequelize.STRING, allowNull: false},
+    password: {type: Sequelize.VIRTUAL},
+    //passwordHash: {type: Sequelize.STRING, allowNull: false},
+    role: {type: Sequelize.ENUM('EMPLOYER','USER'), allowNull:false}
 })
 module.exports = User;
--- utils/seedDatabase.js
+++ utils/seedDatabase.js
@@ -5,17 +5,23 @@ const seedDatabase = async () => {
     User.create({
         firstName: "John",
         lastName: "Doe",
-        phoneNumber: "333-444-5555"
+        phoneNumber: "333-444-5555",
+        email: "john@example.com",
+        role: "USER"
     }),
     User.create({
         firstName: "Jane",
         lastName: "Doe",
-        phoneNumber: "444-555-6666"
+        phoneNumber: "444-555-6666",
+        email: "jane@example.com",
+        role: "USER"
     }),
     User.create({
-        firstName: "John",
+        firstName: "Jack",
         lastName: "Doe",
-        phoneNumber: "555-666-7777"
+        phoneNumber: "555-666-7777",
+        email: "jack@example.com",
+        role: "USER"
     }),
   ]);
 };
