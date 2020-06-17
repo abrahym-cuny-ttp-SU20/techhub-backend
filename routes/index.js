4a1d12c0d2309e1d92b65c3686627146155cfded
Marlon-Urena <45586481+Marlon-Urena@users.noreply.github.com>
2020-06-17 11:29:29 +0100

    Stable registration endpoint completed.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const { user } = require('../controllers')
+
 
 // Subrouters;
+router.post('/register',user.register);
 
 
 // Error handling middleware;
