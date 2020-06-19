d4e85004dd267067ebc6b40ee5985f6f3313632d
Marlon-Urena <45586481+Marlon-Urena@users.noreply.github.com>
2020-06-19 20:15:23 +0100

    Created relationship between models

--- database/models/index.js
+++ database/models/index.js
@@ -2,6 +2,12 @@ const User = require("./user");
 const Skill = require("./skill");
 const PageLink = require("./pageLink");
 
+User.belongsToMany(Skill,{through: 'Possession'});
+Skill.belongsToMany(User, { through: "Possession"});
+
+User.hasMany(PageLink);
+PageLink.belongsTo(User);
+
 module.exports = {
     User, Skill, PageLink
 };
